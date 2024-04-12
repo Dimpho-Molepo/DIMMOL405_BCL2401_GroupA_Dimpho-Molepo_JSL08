1834b33de14170899ae5b4dc567cf171761d9bff
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-04-12 13:06:19 +0200

    Added comments on index.js

--- Singleton Pattern for Bank Branch Management/index.js
+++ Singleton Pattern for Bank Branch Management/index.js
@@ -1,24 +1,33 @@
+// Singleton instance of BankBranch
 let bankBranchInstance = null
 
 class BankBranch {
+    // Constructor function that checks if an instance already exists
     constructor(branchInfo) {
+        // If an instance already exists, return it
         if (!bankBranchInstance) {
+            // If no instance exists, initialize the object with the provided branchInfo
             this.branchInfo = branchInfo;
+            // Set the singleton instance to the newly created object
             bankBranchInstance = this;
         } 
 
         return bankBranchInstance;
     }
 
+    // Getter function to retrieve the branchInfo property
     getBranchInfo() {
-        return this.branchInfo
+        // Return the branchInfo property of the singleton instance
+        return this.branchInfo;
     }
 }
 
+// Create two instances of BankBranch with different branchInfo values
 const branchA = new BankBranch("adbd");
 const branchB = new BankBranch("frgh");
 
 console.log(branchA)
 console.log(branchB)
 
+// Log whether 'branchA' and 'branchB' are the same object
 console.log(`Is 'branchA' and 'branchB' the same object? ${branchA === branchB}`);
